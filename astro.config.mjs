8fa6954b4e2f17c7aa63e9ddf1756a4722715762
d4ken <d-shikencho@techqiao.co.jp>
2024-02-06 21:52:26 +0900

    partytownを導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 import {defineConfig} from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      // Shikiの組み込みテーマから選択（または独自のテーマを追加）
-      theme: 'monokai',
-      // 水平スクロールを防ぐために文字の折り返しを有効にする
-      wrap: true,
+    markdown: {
+        shikiConfig: {
+            // Shikiの組み込みテーマから選択（または独自のテーマを追加）
+            theme: 'monokai',
+            // 水平スクロールを防ぐために文字の折り返しを有効にする
+            wrap: true
+        }
     },
-  },
-  integrations: [preact()]
+    integrations: [preact(), partytown({
+        config: {
+            forward: ['dataLayer.push'],
+        }
+    })]
 });
